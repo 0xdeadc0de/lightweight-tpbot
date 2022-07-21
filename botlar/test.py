5b525c82484cf46417d9ab3b19f77b70eed274dc
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2022-07-21 11:33:58 +0300

    test botunu guncelle ve hizli kapat modu ekle

--- botlar/test.py
+++ botlar/test.py
@@ -4,11 +4,17 @@ yoneticiler = [
 ]
 token_ismi = "TPBOT_TOKEN_TEST"
 
-print(f"[{token_ismi}]", "merhaba dünya")
+import time
+def gunluk(*mesajlar):
+    print(time.strftime("%Y/%m/%d %H:%M (GMT%z)"), f"[{token_ismi}]:", *mesajlar)
+
+gunluk("merhaba dünya")
+import requests
+import subprocess
 import discord
 class MyClient(discord.Client):
     async def on_ready(self):
-        print(f"[{token_ismi}]", 'Atis serbest', self.user)
+        gunluk("Atis serbest", self.user)
 
     async def on_message(self, message):
         
@@ -18,13 +24,17 @@ class MyClient(discord.Client):
 
         if message.content == '!ping':
             await message.channel.send(f'pong [{token_ismi}]')
-            
+            return
+
         if message.content.startswith("!kapan "):
             hedef = message.content[7:]
 
             if hedef == token_ismi:
                 exit(1)
             return
+
+        if message.content == "!kapan testci":
+            exit(1)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -33,6 +43,6 @@ client = MyClient(intents=intents)
 import os
 token = os.environ.get(token_ismi)
 if token is None:
-    print(f"[{token_ismi}]", f"{token_ismi} degeri tanimli degil")
+    gunluk(f"{token_ismi} degeri tanimli degil")
     exit(-1)
 client.run(token)
