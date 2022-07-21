db07cb74924a5e34cc75a8bff039444157d2595f
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2022-07-21 10:34:50 +0300

    token ismi ekle ve print et

--- hepsine_hukmedecek_tek_dosya.py
+++ hepsine_hukmedecek_tek_dosya.py
@@ -1,13 +1,13 @@
-import discord
-
 yoneticiler = [
     824573651390562325, # Kral Risitas
     272044185689915392, # MrChuck
 ]
+token_ismi = "TPBOT_TOKEN_KARANLIKLAR_LORDU"
 
+import discord
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print('Logged on as', self.user, f"[{token_ismi}]")
 
     async def on_message(self, message):
         
@@ -24,7 +24,7 @@ intents.message_content = True
 client = MyClient(intents=intents)
 
 import os
-token = os.environ.get("TPBOT_TOKEN")
+token = os.environ.get(token_ismi)
 if token is None:
     print("TPBOT_TOKEN degeri tanimli degil")
     exit(-1)
