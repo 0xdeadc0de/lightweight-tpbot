4cd2f0e4f0a41414135e50956335e7c2bf97c7e7
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2022-08-07 19:04:52 +0300

    nazgul arayu guncelle

--- botlar/arayuz_nazgul.py
+++ botlar/arayuz_nazgul.py
@@ -24,10 +24,28 @@ Simsiyah pelerin ve çarşafa bürünmüş bu varlık yavaşçana suratını san
 
         @discord.ui.button(label="Sihirli bir kartı nasıl oynayabilirim?", style=discord.ButtonStyle.secondary, row=1)
         async def _2(self, button, interaction: discord.Interaction):
+            class Devam(discord.ui.View):
+                def __init__(self, *items, timeout = None, bot = None):
+                    self.bot = bot
+                    super().__init__(*items, timeout=timeout)
+                @discord.ui.button(label="Bahsettiğin güç hakkında bilgi verebilir misin?", style=discord.ButtonStyle.secondary, row=0)
+                async def _1(self, button, interaction: discord.Interaction):
+                    await interaction.response.send_message(
+                        ephemeral=True,
+                        embed=embed_sohbet(self.bot.user, konusma=
+f"""Bu güç Türk Programcılar diyarındaki diğer kıymet teşkil eden madenlere benzemez, senin içine nüfus eder ve \
+hiçkimse senden bu gücü alamaz veya gasp edemez. `Karanlık Güç Token (KGT)`'ına sahip kişiler ancak bu güce erişebilirler ve
+bu güç onları daha da saldırgan hale getirir.
+
+Gün aşırı <#{Kanal.BugunNeYaptim.id}> diyarındaki gönderisi üzerine açılan başlığa ` !kgt ` komutunu \
+çalıştırdığı zaman 23 saat içerisinde bir Nazgul'ün karanlık gölgesi tarafından sarmalanır ve \
+bu token ona nüfus eder."""))
+
             await interaction.response.send_message(
+                view=Devam(bot=self.bot),
                 ephemeral=True,
                 embed=embed_sohbet(self.bot.user, konusma=
-"""Öncelikle sihirli bir kart oynamak için yeterli gücün olduğuna emin olmalısın.
+"""Öncelikle bazı kartlar için yeterli gücün olduğuna emin olmalısın. Çoğu barışçıl kart için buna ihtiyacın olmayacak.
 
 Daha sonra kart numaranı belirledikten sonra ` /kart ` yazıp oynamak istediğin kart numarasını yazmalısın. \
 Kartı oynamadan önce göz atmak için ` /gozat ` komutunu kullanabilirsin. Böylece doğru kartı oynadığından emin olabilirsin.
