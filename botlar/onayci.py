4d9dfa554f0f3ecf488d1630d213b7ee7f0c1ac7
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2022-08-19 15:38:37 +0300

    sag tik onay ver mesaja

--- botlar/onayci.py
+++ botlar/onayci.py
@@ -22,7 +22,7 @@ Cevaplarınızı verdikten sonra, cevaplarınızda bir eksik yok ise kaydınız 
 -TP Yönetim <@325442322986696715> <@1001882496549527553>"""
 
 import discord
-from alayina_gider import Cogcu, Kanal, thumbs_down, thumbs_up
+from alayina_gider import Cogcu, Kanal, thumbs_down, thumbs_up, idler, Rol
 from discord.ext.commands import *
 
 class Onayci(Cogcu): 
@@ -39,6 +39,11 @@ class Onayci(Cogcu):
         await baslik.send(f"<@{ctx.author.id}> "+padisah_fermani)
         await ctx.delete()
 
+    async def onay_ver(self, uye: discord.member.Member):
+
+        tp_uyesi_rolu = discord.utils.get(uye.guild.roles, id=900647464342790204)
+        await uye.add_roles(tp_uyesi_rolu)
+
         
     @command()
     @check(izinli_rollu_mu)
@@ -50,10 +55,10 @@ class Onayci(Cogcu):
     async def onay(self, ctx: Context):
         if len(ctx.message.mentions) > 0:
             try:
+                
                 uye: discord.member.Member = ctx.message.mentions[0]
                 
-                tp_uyesi_rolu = discord.utils.get(uye.guild.roles, id=900647464342790204)
-                await uye.add_roles(tp_uyesi_rolu)
+                await self.onay_ver(uye)
             
                 await thumbs_up(ctx)
             except:
@@ -63,6 +68,21 @@ class Onayci(Cogcu):
 
         if ctx.channel.parent_id == Kanal.MerhabaDunya.id:
            await ctx.channel.archive(locked=True)
+
+           
+    @message_command(guild_ids=[idler.sunucu], name="onay ver")
+    @Rol.AsistanEditorKurucuMu()
+    async def caliskanim(self, ctx: discord.ApplicationContext, message: discord.message.Message):
+        
+        try:
+            await self.onay_ver(message.author)
+            msg="onay verildi: "
+        except:
+            msg="onay verilemedi: "
+
+        msg+=message.author.display_name
+        
+        await ctx.respond(msg, ephemeral=True)
 
 def main():
     Onayci("TPBOT_TOKEN_NAZGUL_1", Onayci.__name__.lower())
